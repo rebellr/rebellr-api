1c87e7ea9029f58421ad46602fd52516cec5edea
Saketram Durbha <durbhasaketram@gmail.com>
2016-12-25 15:24:07 -0700

    Make format for all requests JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :format_json
+
+  def format_json
+    request.format = 'json'
+  end
 
   def render_unauthorized(reason)
     self.headers['WWW-Authenticate'] = 'Token realm="Application"'
