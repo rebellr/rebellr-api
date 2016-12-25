f3d07dc694909b5495e16c2cb3695c3ee1a7a145
Saketram Durbha <durbhasaketram@gmail.com>
2016-12-25 12:51:03 -0700

    Revert rename auth_token to authentication_digest

--- db/migrate/20161225194327_rename_column_auth_token_in_sessions_to_authentication_digest.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnAuthTokenInSessionsToAuthenticationDigest < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :sessions, :auth_token, :authentication_digest
-  end
-end
