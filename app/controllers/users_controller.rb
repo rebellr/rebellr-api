48610a61f17381bdf047b28ed6a67ffcf88568da
Saketram Durbha <durbhasaketram@gmail.com>
2016-12-25 10:40:24 -0700

    Fix password mistake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  has_secure_password
 
   # GET /users
   # GET /users.json
@@ -49,6 +48,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :school_id)
+      params.require(:user).permit(:first_name, :last_name, :email, :school_id, :password, :password_confirmation)
     end
 end
